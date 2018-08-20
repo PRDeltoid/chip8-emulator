952c2a33f91f9cc002d8f05a59bfe61a309d0fe1
tbritton <tbrit10@gmail.com>
2018-08-20 11:16:54 -0700

    Added ROM loader and starting opcode interpreter
    Made chip8 an object instead of a set of functions
    Added a basic Match statement to match some  starting opcodes
    Added the ability to load a ROM during initialization to populate
    memory

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,20 @@ V Regs are 1 byte long (u8)
 Opcodes are 2 bytes long (u16). This means we must combine 2 1-byte numbers in memory into a single 2-byte number
 This is done by rotating the leading number (the big end) by 8 bits. This will create a 2-byte number with 1 byte of zeros at the little end
 We then bitwise OR our 2 byte number and our 1-byte number that we want to combine. All of the 1s in the 1-byte number are kept in the final result
+
+Rust does not allow Hex literals in code, so most hex will be converted to decimal before being entered.
+Their decimal equivalence and purpose should be noted in the comments or via constants
 ************/
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+const FIRST_NIBBLE_MASK: u16 = 61440;  //0xF000, grabs first nibble only
+const SECOND_NIBBLE_MASK: u16 = 3840;  //0x0F00, grabs second nibble only
+const THIRD_NIBBLE_MASK: u16 = 240;    //0x00F0, ...
+const FOURTH_NIBBLE_MASK: u16 = 15;    //0x000F, ...
+
+const LAST_THREE_MASK: u16 = 4095;     //0x0FFF, grabs last three nibbles only
 
 pub struct Chip8 {
     opcode: u16,        //Opcode
@@ -16,7 +29,7 @@ pub struct Chip8 {
     v: [u8; 16],        //General purpose registers. Register 16 is the "carry flag"
 
     i: u16,             //Index register
-    pc: usize,            //Program counter (instruction pointer)
+    pc: u16,            //Program counter (instruction pointer)
 
     screen: [u8; 64 * 32], //Array for storing screen pixels. Screen is 64 x 32 pixels
 
@@ -32,11 +45,11 @@ pub struct Chip8 {
 impl Chip8 {
     pub fn new() -> Chip8 {
         Chip8 {
-            opcode: 0,
-            memory: [0; 4096],
-            v: [0; 16],
+            opcode: 0,         //Blank opcode
+            memory: [0; 4096], //Initialize our memory
+            v: [0; 16],        //Zero out our registers
             i: 0,
-            pc: 0,
+            pc: 512,           //program counter starts at 0x200 (system data comes before)
             screen: [0; 64 * 32],
             delay_timer: 0,
             sound_timer: 0,
@@ -45,47 +58,82 @@ impl Chip8 {
             key: [0; 16],
         }
     }
+
+    pub fn initialize(&mut self, rom_path: &str) {
+        let mut rom = File::open(rom_path).unwrap();
+        let mut i = 512;
+
+        for byte in rom.bytes() {
+            self.memory[i] = byte.unwrap();
+            i += 1;
+        }
+
+        for (i, byte) in self.memory.iter() .enumerate() {
+            if *byte != 0 {
+                println!("{}: {}", i, byte);
+            }
+        }
+    }
+
+    pub fn emulate_cycle(&mut self) {
+        //Fetch opcode
+
+        //Grab the first half of the opcode as 2-byte, shifted 8 bits left
+        let opcode1: u16 = (self.memory[self.pc as usize] as u16) << 8;
+        //Grab second half of opcode as 2-byte
+        let opcode2: u16 = self.memory[(self.pc+1) as usize] as u16;
+        //OR the two two-byte numbers (one "big end" and one "small end") to combine them
+        let opcode = opcode1 | opcode2;
+
+        println!("Final opcode is {:}", opcode); //Testing output
+
+        //decode and execute opcode
+        //our first hex digit (nibble) mask, 0xf000 is 61440 in decimal
+        match opcode & FIRST_NIBBLE_MASK {
+            //0xANNN opcode (mv i, NNN)
+            40960 => {
+                self.i = opcode & LAST_THREE_MASK;  //4095 is 0x0fff in hex (our mask to grab xxx from above)
+                self.pc += 2;
+                println!("changing index address to {:}", self.i)
+            },
+            //0xBNNN opcode (jump to location at NNN + V0)
+            45056 => {
+                self.pc = (opcode & LAST_THREE_MASK) + self.v[0] as u16;
+
+            }
+            default => {
+                println!("Unknown opcode {:}", opcode);
+            },
+        }
+
+        //Update timer(s)
+        if self.delay_timer > 0 {
+            self.delay_timer -= 1;
+        }
+
+        if self.sound_timer > 0 {
+            if self.sound_timer == 1 {
+                //Make a beep noise
+            }
+            self.sound_timer -= 1;
+        }
+    }
 }
 
 fn main() {
-    let mut chip8= Chip8::new();
+    //Create and initialize our Chip8 object
+    let mut chip8 = Chip8::new();
 
-    chip8.memory[0] = 161; //A1 in Hex
-    chip8.memory[1] = 35;  //23 in Hex
+    chip8.initialize("pong.rom");
 
-    emulate_cycle(&mut chip8);
+    //Manually load in some opcodes for testing
+    //Program memory starts at address 512 (0x200)
+    /*chip8.memory[512] = 161; //A1 in Hex
+    chip8.memory[513] = 35;  //23 in Hex*/
 
-}
+    //Our memory now contains [A1, 23]. This is the opcode A123.
+    //This is the opcode for "move instruction pointer to address 0x123" (0xANNN, where NNN is the address)
 
-
-fn _initialize(_chip8: &mut Chip8) {
-    //Initialize the registers and counters on the chip
-}
-
-fn emulate_cycle(chip8: &mut Chip8) {
-    //Fetch opcode
-
-    //Grab the first half of the opcode as byte
-    let mut opcode8: u8 = chip8.memory[chip8.pc];
-    //Cast byte as 2 bytes
-    let mut opcode = opcode8 as u16;
-    //Shift the first half by 8 bits, so first byte is now is the "big end" of a 2 byte number
-    opcode = opcode << 8;
-    println!("Shifted opcode is {:}", opcode);
-    //Grab next byte
-    opcode8 = chip8.memory[chip8.pc+1];
-    //Convert to 2-byte number (again)
-    let mut opcode2: u16 = opcode8 as u16;
-
-    //OR the two two-byte (one "big end" and one "small end") to combine them
-    opcode = opcode | opcode2;
-    println!("Final opcode is {:}", opcode);
-
-    //Increase program counter by 2 (since we just consumed two bytes)
-    chip8.pc = chip8.pc + 2
-
-    //Decode it
-    //Execute it
-
-    //Update timer(s)
+    //Emulate a CPU cycle
+    chip8.emulate_cycle();
 }
